e8abb029ff1b0bdefc5edcb027f08713afe7e8c2
unknown <benaremail@gmail.com>
2021-12-14 11:11:41 +0800

    m

--- dcsfe/src/components/PatientTable2.js
+++ dcsfe/src/components/PatientTable2.js
@@ -7,8 +7,49 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 const PatientTable2 = () => {
     return (
-        <div>
-            
+        <div className='patient-table-container'>
+
+            <div className='patient-table-head-container'>
+                <div className='patient-table-head-title'>Patient Table</div>
+                <div className='patient-table-head-input'>
+                    <div className='patient-table-head-search-container'>
+                        <button className='patient-table-head-search-button'>Search</button>
+                        <input className='patient-table-head-search-input' />
+                        <button className='patient-table-head-search-clear'>X</button>
+                    </div>
+                </div>
+                <div className='patient-table-head-add-container'>
+                    <button className='patient-table-head-add-button'></button>
+                </div>
+            </div>
+
+            <div className='patient-table-table'>
+                <thead className='patient-table-table-thead'>
+                    <tr className='patient-table-table-thead-tr'>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Details</th>
+                        <th>Treatment</th>
+                    </tr>
+                </thead>
+                <tbody className='patient-table-table-tbody'>
+                    <tr className='patient-table-table-tbody-tr'>
+                        <td>1</td>
+                        <td>Benar F Isais</td>
+                        <td>
+                            <span>Active</span>
+                        </td>
+                        <td>
+                            <button>Details</button>
+                        </td>
+                        <td>
+                            <button>Treatment</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </div>
+
         </div>
     )
 }
